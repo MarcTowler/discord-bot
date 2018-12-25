3bb5c61003f29344a0d9835492ecf7e54e9d100b
Marc Towler <marc@marctowler.co.uk>
2018-12-25 23:57:06 +0000

    Fixed an issue with addtwo() that was throwing errors

--- commands/addtwo.js
+++ commands/addtwo.js
@@ -1,4 +1,12 @@
-function addtwo(x, y) {
-    return x + y;
+const Discord = require("discord.js");
+const https = require('https');
+
+module.exports.run = async(bot, message, args) => {
+    function addtwo(x, y) {
+        return x + y;
+    }
 }
-module.exports = addtwo;
+
+module.exports.help = {
+    name: "addtwo"
+}
