bb1eadb4166a7e45eb93443176d3c61911d4b6a4
Marc Towler <marc@marctowler.co.uk>
2018-12-26 00:03:00 +0000

    update package.json

--- commands/addtwo.js
+++ commands/addtwo.js
@@ -1,12 +1,7 @@
-const Discord = require("discord.js");
-const https = require('https');
-
-module.exports.run = async(bot, message, args) => {
     function addtwo(x, y) {
         return x + y;
     }
     module.exports = addtwo;
-}
 
 module.exports.help = {
     name: "addtwo"
