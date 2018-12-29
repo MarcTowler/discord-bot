00769d976570b5c65754f9889063e52c91110a76
Marc Towler <marc@marctowler.co.uk>
2018-12-29 01:40:11 +0000

    Updated archive to only work with "test rank" members Updated poll to work differently depending on if 1 or 2 args were given and what they were, including basic error handling Added basic poll command

--- commands/points.js
+++ commands/points.js
@@ -2,36 +2,74 @@ const Discord = require("discord.js");
 const https = require('https');
 
 module.exports.run = async(bot, message, args) => {
-    if (args[0] === 'pve' && args[1] === 'top10') {
-        const embed = new Discord.RichEmbed()
-        // Set the title of the field
-            .setTitle('Top 10 PvE Points')
-            // Set the color of the embed
-            .setColor(0xFF0000)
-            // Set the main content of the embed
-            .setDescription('1) TestName (1,000)\n2) King Diddy (800)');
+    if(args.length <= 0)
+    {
+        return message
+            .channel
+            .send(`Something didn't quite go right, !${this.help.triggers} requires PvE or PvP as a minimum argument. Try again with !${this.help.triggers} PvE or !${this.help.triggers} PvP`);
+    }
+    
+    //Lets make sure they used pve or pvp for the right leaderboards...
+    if(args[0].toLowerCase() === "pvp" || args[0].toLowerCase() === "pve")
+    {
+        switch (args.length)
+        {
+            //can either be a top 10 call OR a user calling someone else's stats
+            case 2:
+                //lets check for top10
+                if(args[1].toLowerCase() === 'top10')
+                {
+                    //TBC
+                    console.log('top10 called');
+                } else {
+                    //It should be calling another user's stats
+                    https.get(`https://api.itslit.uk/G4G/getList/1/${args[0].toLowerCase()}/${args[1].toLowerCase()}/${message.author.username}/plain/true`, (resp) => {
+                        let data = '';
 
-        // Send the embed to the same channel as the message
-        message.channel.send(embed);
-    } else if(args[0] === "pve") {
-        https.get('https://api.itslit.uk/G4G/getList/1/' + args[0] + '/null/' + message.author.username + '/plain/true', (resp) => {
-            let data = '';
+                        resp.on('data', (chunk) => {
+                            data += chunk;
+                        });
 
-            //a chunk of data has been received
-            resp.on('data', (chunk) => {
-                data += chunk;
-            });
+                        resp.on('end', () => {
+                            return message.channel.send(data.toString());
+                        });
+                    }).on("error", (err) => {
+                        console.log(`Error: ${err.message}`);
+                    });
+                }
+                break;
+            //Calling their own stats
+            case 1:
+                //Quick check to make sure that PvE or PvP is set
+                https.get(`https://api.itslit.uk/G4G/getList/1/${args[0].toLowerCase()}/null/${message.author.username}/plain/true`, (resp) => {
+                    let data = '';
 
-            //the whole response is here
-            resp.on('end', () => {
-                message.channel.send(data.toString());
-            });
-        }).on("error", (err) => {
-            console.log("Error: " + err.message);
-        });
+                    resp.on('data', (chunk) => {
+                        data += chunk;
+                    });
+
+                    resp.on('end', () => {
+                        return message.channel.send(data.toString());
+                    });
+                }).on("error", (err) => {
+                    console.log(`Error: ${err.message}`);
+                });
+
+                break;
+            default: //Something went wrong
+                return message
+                    .channel
+                    .send(`Something didn't quite go right, !${this.help.triggers} requires PvE or PvP as a minimum argument. Try again with !${this.help.triggers} PvE or !${this.help.triggers} PvP`);
+        }
+    } else {
+        return message
+            .channel
+            .send(`Something didn't quite go right, !${this.help.triggers} requires PvE or PvP as a minimum argument. Try again with !${this.help.triggers} PvE or !${this.help.triggers} PvP`);
     }
 }
 
 module.exports.help = {
-    name: "points"
+    name: "points",
+    triggers: "points",
+    description: "See where you stand in G4G's PvE or PvP clan leaderboards!"
 }
---
+++ commands/poll.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+    //!poll QUESTION OPT1 OPT2 OPT3 etc up to 20
+    //!poll "question" "option" "option"
+    let options = message.content.match(/"(.+?)"/g);
+
+    //Did we get anything?
+    if(options) {
+        //spam protection maybe?
+        //are we having a yes/no poll?
+        if(options.length === 1) {
+            const question = options[0].replace(/"/g, '');
+            //const question = args[0];
+
+            return message
+                .channel
+                .send(`${message.author} asks: ${question}`)
+                .then(async (pollMessage) => {
+                    await pollMessage.react('✅');
+                    await pollMessage.react('❎');
+                });
+        }
+    }
+    //return message.channel.send(`Poll command is in development`);
+    //return message.channel.send(`Poll command is in development`);
+}
+
+module.exports.help = {
+    name: "poll",
+    triggers: "poll",
+    description: "Create a poll! Votes handled via emoji reaction"
+}
