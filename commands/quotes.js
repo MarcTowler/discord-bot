5beaba0e26c8bb018c2bceaf39ae19bdeed55864
Marc Towler <marc@marctowler.co.uk>
2019-01-09 22:36:21 +0000

    Solved #22 (#23)
    * Added feature for #22, quote removal

--- commands/quotes.js
+++ commands/quotes.js
@@ -39,11 +39,37 @@ module.exports.run = async(bot, message, args) => {
                     message.channel.send(data);
                 });
             }).on("error", (err) => {
-                message.channel.send(`It seems that something has gone wrong, <@131526937364529152> has been notified and is looking into it.`);
-                message.guild.fetchMember('131526937364529152').then(user => {user.send(`A new error has occured in ${message.channel.name} caused by ${message.author.username}` +
-                    ` using !${this.help.triggers} ${args}.\n The Error was ${err.message}`)});
+                message.guild.fetchMember('131526937364529152').then(user => {
+                    user.send(`A new error has occured in ${message.channel.name} caused by ${message.author.username}` +
+                        ` using !${this.help.triggers} ${args}.\n The Error was ${err.message}`).then(
+                        message.channel.send(`It seems that something has gone wrong, <@131526937364529152> has been notified and is looking into it.`)
+                    )
+                });
             });
 
+        } else if(args[0] === 'remove') {
+            let toSend = args.join(" ").replace('remove ', '');
+
+            https.get(`https://api.itslit.uk/Lists/remove/item/G4G/quotes/${toSend}/true`, (resp) => {
+                let data = '';
+
+                //a chunk of data has been received
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                });
+
+                //the whole response is here
+                resp.on('end', () => {
+                    message.channel.send(data);
+                });
+            }).on("error", (err) => {
+                message.guild.fetchMember('131526937364529152').then(user => {
+                    user.send(`A new error has occured in ${message.channel.name} caused by ${message.author.username}` +
+                        ` using !${this.help.triggers} ${args}.\n The Error was ${err.message}`).then(
+                        message.channel.send(`It seems that something has gone wrong, <@131526937364529152> has been notified and is looking into it.`)
+                    )
+                });
+            });
         }
     }
 }
