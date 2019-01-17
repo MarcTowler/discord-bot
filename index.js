646e30aa4a3cc0bbf04ef9a504f068152c41a880
Marc Towler <marc@marctowler.co.uk>
2019-01-17 15:37:35 +0000

    Dev (#26)
    * Modified Points Embed output at Kayowin's request to tidy it up a bit more
    
    * Changed the Welcome slightly

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const config = require('./botconfig.json');
 const Discord = require('discord.js');
 const fs = require("fs");
+const config = require('./botconfig.json');
 
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
@@ -44,7 +44,20 @@ bot.on("ready", async () => {
         const index = Math.floor(Math.random() * (activity_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
         bot.user.setActivity(activity_list[index]); // sets bot's activities to one of the phrases in the arraylist.
     }, 60000);
+
+    Notify_chan = bot.channels.find('id', '309597400434212865'); //Get the channel ID for #hellbot_thunderdome
 });
+
+setInterval(function() {
+   let d = new Date();
+   if(Math.floor((d.getTime() - start_time) / 3600000) % interval_hours > 0)
+   {
+       console.log(Math.floor((d.getTime() - start_time) / 3600000) % interval_hours > 0);
+   } else {
+       console.log(Math.floor((d.getTime() - start_time) / 3600000) % interval_hours > 0);
+   }
+    //Notify_chan.sendMessage('g upcoming');
+}, 60 * 1000 * 60 * 72);
 
 bot.on("message", async message => {
     if(message.author.bot) return;
@@ -53,7 +66,7 @@ bot.on("message", async message => {
     if(config.debug === true && message.author.id !== '131526937364529152') return;
     let prefix = config.prefix;
     let messageArray = message.content.replace(/  +/g, ' ').split(" ");
-    let command = messageArray[0];
+    let command = messageArray[0].toLowerCase();
 
     if(command.charAt(0) !== prefix) return;
 
@@ -70,7 +83,6 @@ bot.on("message", async message => {
 bot.on("guildMemberAdd", member => {
     let REmbed = new Discord.RichEmbed()
         .setThumbnail("https://i.imgur.com/IdGhpsp.png")
-        .setAuthor(`Welcome to G4G ${member}`)
         .setColor(5233919)
         .setTitle(`To gain membership and full access to the G4G server, please perform the following steps:`)
         .setDescription(`1: Set your Discord nickname for this server to your Bungie gamer tag or B.net ID\n` +
@@ -78,12 +90,14 @@ bot.on("guildMemberAdd", member => {
             `3: Confirm the Bungie Clan you have joined, eg G4G Orion\n` +
             `4: Accept the invitation to join our Guilded page, authorizing with your Discord details`);
 
-    //if(member.guild.id === "220467406559117312") { //G4G
+    if(member.guild.id === "220467406559117312") { //G4G
     //if(member.guild.id === "236484268237258752") { //GamingHaven
 
-        //member.guild.channels.get('525270659438215178').send(REmbed);
-       // member.guild.channels.get('295671375773958145').send(REmbed);
-    //}
+        //member.guild.channels.get('295671375773958145').send(REmbed); //GH
+        member.guild.channels.get('525270659438215178').send(`Welcome to G4G <@${member.user.id}>`)
+            .then(msg =>
+                member.guild.channels.get('295671375773958145').send(REmbed));
+    }
 });
 
 bot.login(config.token);
